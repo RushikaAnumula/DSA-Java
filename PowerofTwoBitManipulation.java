a60a130e9f47156950ea5409d35e88f843c07078
Rushika Anumula <rushikaanumula2006@gmail.com>
2025-08-09 17:45:24 +0530

    Create PowerofTwoBitManipulation.java

---
+++ PowerofTwoBitManipulation.java
@@ -0,0 +1,25 @@
+/*Given an integer n, return true if it is a power of two. Otherwise, return false.
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+Example 1:
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+Input: n = 3
+Output: false*/
+import java.util.*;
+class PowerofTwoBitManipulation{
+  public boolean isPowerOfTwo(int n){
+    return n>0 && (n & (n-1))==0;
+  }
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		PowerofTwoRec obj=new PowerofTwoRec();
+		System.out.print(obj.isPowerOfTwo(n));
+	}
+}
