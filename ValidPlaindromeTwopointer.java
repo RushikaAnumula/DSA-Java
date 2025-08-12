8e2766272cd10c3e7dc6dc583e18de34920dff7f
Rushika Anumula <rushikaanumula2006@gmail.com>
2025-08-12 22:44:52 +0530

    Create ValidPlaindromeTwopointer.java

---
+++ ValidPlaindromeTwopointer.java
@@ -0,0 +1,40 @@
+/*A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.*/
+import java.util.*;
+class ValidPlaindromeTwopointer{
+  public boolean isPalindrome(String s){
+    StringBuilder chars=new StringBuilder();
+    for(char c:s.toCharArray()){
+      if(Character.isLetterOrDigit(c)){
+        chars.append(Character.toLowerCase(c));
+      }
+    }
+    int left=0,right=chars.length()-1;
+    while(left<right){
+      if(chars.charAt(left)!=chars.charAt(right)) return false;
+      left++;
+      right--;
+    }
+    return true;
+  }
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		ValidPlaindromeTwopointer obj=new ValidPlaindromeTwopointer();
+		System.out.print(obj.isPalindrome(s));
+	}
+}
+}
