8c2d39ea5e3699966b0b0c297e151407e49d16ec
Rushika Anumula <rushikaanumula2006@gmail.com>
2025-09-07 21:15:04 +0530

    Create MajorityElementHashMap.java

---
+++ MajorityElementHashMap.java
@@ -0,0 +1,26 @@
+/*Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2*/
+import java.util.*;
+class MajorityElementHashMap{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] nums=new int[n];
+		for(int i=0;i<n;i++){
+			nums[i]=sc.nextInt();
+		}
+		HashMap<Integer,Integer> map=new HashMap<>();
+		for(int num:nums){
+			map.put(num,map.getOrDefault(num,0)+1);
+			if(map.get(num)>n/2){
+				System.out.print(num);
+			}
+		}
+	}
+}
