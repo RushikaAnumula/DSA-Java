a2b96d841fa71231147064180c001b036f59672c
Rushika Anumula <rushikaanumula2006@gmail.com>
2025-12-29 19:32:57 +0530

    Update MajorityElementHashMap.java

--- MajorityElementHashMap.java
+++ MajorityElementHashMap.java
@@ -16,12 +16,9 @@ class MajorityElementHashMap{
 			nums[i]=sc.nextInt();
 		}
 		HashMap<Integer,Integer> map=new HashMap<>();
-		for(int num:nums){
-			if(map.containsKey(num)){
-				map.put(num,map.get(num)+1);
-			}else{
-				map.put(num,1);
-			}
+		for (int num:nums){
+			if(map.containsKey(num)) map.put(num,map.get(num)+1);
+			else map.put(num,1);
 			if(map.get(num)>n/2){
 				System.out.print(num);
 		}
