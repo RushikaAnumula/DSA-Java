ea891bcf40a3b7ad997583f637f56a5aa4ff285b
Rushika Anumula <rushikaanumula2006@gmail.com>
2026-01-22 21:31:56 +0530

    Create LongestSubstringWithoutRepeatingCharactersSlidingWindow.java

---
+++ LongestSubstringWithoutRepeatingCharactersSlidingWindow.java
@@ -0,0 +1,41 @@
+/*Given a string s, find the length of the longest substring without duplicate characters.
+Example 1:
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+Example 2:
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+Constraints:
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.*/
+import java.util.*;
+class LongestSubstringWithoutRepeatingCharactersSlidingWindow{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		System.out.print(lengthOfLongestSubstring(s));
+	}
+	public static int lengthOfLongestSubstring(String s){
+		int maxLen=0;
+		int l=0,r=0;
+		HashSet<Character> set=new HashSet<>();
+		while(r<s.length()){
+			char c=s.charAt(r);
+			while(set.contains(c)){
+				set.remove(s.charAt(l));
+				l++;
+			}
+			set.add(c);
+			maxLen=Math.max(maxLen,r-l+1);
+			r++;
+		}
+		return maxLen;
+	}
+}
