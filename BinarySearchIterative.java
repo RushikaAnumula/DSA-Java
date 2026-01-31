b3d50ef1b77576c1f061573d65e3f544c0d667a1
Rushika Anumula <rushikaanumula2006@gmail.com>
2026-01-31 18:35:40 +0530

    Update BinarySearchIterative.java

--- BinarySearchIterative.java
+++ BinarySearchIterative.java
@@ -11,28 +11,28 @@ Explanation: 2 does not exist in nums so return -1*/
 import java.util.*;
 class BinarySearchIterative {
   public static int search(int[] nums, int target) {
-    int low = 0;
-    int high = nums.length - 1;
-    while (low <= high) {
-      int mid = low + (high - low) / 2;
-            if (nums[mid] == target)
+    int low=0;
+    int high=nums.length-1;
+    while(low<=high){
+      int mid=low+(high-low)/2;
+            if(nums[mid]==target)
                 return mid;
-            else if (nums[mid] < target)
-                low = mid + 1;
+            else if(nums[mid]<target)
+                low=mid+1;
             else
-                high = mid - 1;
+                high=mid-1;
         }
         return -1;
     }
     public static void main(String[] args){
-      Scanner sc = new Scanner(System.in);
-      int n = sc.nextInt();
-      int[] nums = new int[n];
-      for (int i = 0; i < n; i++) {
-        nums[i] = sc.nextInt();
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int[] nums=new int[n];
+      for(int i=0;i<n;i++) {
+        nums[i]=sc.nextInt();
       }
-      int target = sc.nextInt();
-      int result = search(nums, target);
+      int target=sc.nextInt();
+      int result=search(nums, target);
       System.out.println(result);
     }
 }
