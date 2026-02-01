9037219aa24c22796891b7c7ac8316542cd3ab38
Rushika Anumula <rushikaanumula2006@gmail.com>
2026-02-01 10:06:27 +0530

    Update MaximumAverageSubarrayISlidingWindow.java

--- MaximumAverageSubarrayISlidingWindow.java
+++ MaximumAverageSubarrayISlidingWindow.java
@@ -24,19 +24,20 @@ class MaximumAverageSubarrayISlidingWindow{
 		System.out.print(findMaxAverage(nums,n,k));
 	}
 	public static double findMaxAverage(int[] nums,int n,int k){
-        int sum=0;
+		int n=nums.length;
+		int sum=0;
         for(int i=0;i<k;i++){
-            sum+=nums[i];
-        }
-        double maxAvg=(double)sum/k;
-        for(int i=k;i<n;i++){
-            sum+=nums[i]-nums[i-k];
-            double avg=(double)sum/k;
-            if(avg>maxAvg){
-                maxAvg=avg;
-            }
-        }
-        return maxAvg;
+			sum+=nums[i];
+		}
+		double maxAvg=(double)sum/k;
+		for(int i=k;i<n;i++){
+			sum+=nums[i]-nums[i-k];
+			double avg=(double)sum/k;
+			if(avg>maxAvg){
+				maxAvg=avg;
+			}
+		}
+		return maxAvg;
 	}
 }
 	
