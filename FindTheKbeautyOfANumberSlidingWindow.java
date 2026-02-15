3fc9b3efd453a0bf82281a51105099fd070fb9a3
Rushika Anumula <rushikaanumula2006@gmail.com>
2026-02-15 10:29:47 +0530

    Update FindTheKbeautyOfANumberSlidingWindow.java

--- FindTheKbeautyOfANumberSlidingWindow.java
+++ FindTheKbeautyOfANumberSlidingWindow.java
@@ -37,7 +37,7 @@ class FindTheKbeautyOfANumberSlidingWindow{
 	public static int divisorSubstrings(int num, int k){
 		String s=Integer.toString(num);
 		int n=s.length();
-		int i=0;count=0;
+		int i=0,count=0;
 		for(int j=0;j<n;j++){
 			if(j-i+1==k){
 				int subStr=Integer.parseInt(s.substring(i,j+1));
