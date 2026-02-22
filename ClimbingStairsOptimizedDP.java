4172ed1c478d1cdb739562877c2954f1a66b547d
Rushika Anumula <rushikaanumula2006@gmail.com>
2026-02-22 18:50:36 +0530

    Update ClimbingStairsOptimizedDP.java

--- ClimbingStairsOptimizedDP.java
+++ ClimbingStairsOptimizedDP.java
@@ -24,9 +24,9 @@ class ClimbingStairsOptimizedDP{
 	}
 	public static int climbStairs(int n){
 		if(n<=1) return 1;
-		int prev2=1, prev1=1;
+		int prev2=1,prev1=1;
 		for(int i=2;i<=n;i++){
-			int curr=prev1+prev2;
+			int curr=prev2+prev1;
 			prev2=prev1;
 			prev1=curr;
 		}
